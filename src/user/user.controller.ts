bd54c2cbcba2a2bf506f89f79cbe10927ef9b687
Cookie_gg <cookie.gg.dev@gmail.com>
2022-05-08 04:08:31 +0900

    add repositories

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,19 +1,45 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CreateUserInput, User } from './user.model';
 
 @ApiTags('user')
 @Controller('user')
 export class UserController {
-  @Post('create')
+  @Post()
   @ApiOperation({ summary: 'Create user' })
   createUser(@Body() body: CreateUserInput): User {
     return body;
   }
 
-  @Get('all')
+  @Get()
   @ApiOperation({ summary: 'Get all users' })
   getAllUsers(): User[] {
     return [];
+  }
+
+  @Get(':id')
+  @ApiOperation({ summary: 'Get user by id' })
+  getUser(@Param() params: { id?: string }): string {
+    return params.id;
+  }
+
+  @Delete(':id')
+  @ApiOperation({ summary: 'Delete user by id' })
+  deleteUser(@Param() params: { id?: string }): string {
+    return params.id;
+  }
+
+  @Put(':id')
+  @ApiOperation({ summary: 'Update user by id' })
+  updateUser(@Body() body: CreateUserInput): User {
+    return body;
   }
 }
